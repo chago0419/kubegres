6678797996df3e04263008cf9b3e446b4ba8e128
chago <53162662+chago0419@users.noreply.github.com>
2021-05-17 00:29:30 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+kubectl apply -f https://raw.githubusercontent.com/reactive-tech/kubegres/v1.1/kubegres.yaml
+kubectl apply -f my-postgres-secret.yaml
+sudo mkdir /mnt/disks/vol1 /mnt/disks/vol2 /mnt/disks/vol3
+kubectl apply -f sc-lv.yaml
+kubectl apply -f pv-lv.yaml
+kubectl apply -f my-postgres.yaml
